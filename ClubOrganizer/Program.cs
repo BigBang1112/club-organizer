4f15c15ccbda204407a6def1a9aef0adbf97a5fb
Petr Pivoňka <petrpiv1@gmail.com>
2024-06-23 21:36:20 +0200

    Add basic club retrieval test

---
+++ ClubOrganizer/Program.cs
@@ -0,0 +1,22 @@
+﻿using ManiaAPI.NadeoAPI;
+using Spectre.Console;
+
+using var nls = new NadeoLiveServices();
+
+var login = AnsiConsole.Ask<string>("Enter Ubisoft Connect [green]login[/]:");
+
+var password = AnsiConsole.Prompt(
+    new TextPrompt<string>("Enter Ubisoft Connect [green]password[/]:")
+        .PromptStyle("red")
+        .Secret());
+
+await nls.AuthorizeAsync(login, password, AuthorizationMethod.UbisoftAccount);
+
+AnsiConsole.MarkupLine("[green]Authorization successful[/]");
+
+var clubs = await nls.GetMyClubsAsync(20);
+
+foreach (var club in clubs.ClubList)
+{
+    AnsiConsole.WriteLine(club.Name);
+}
